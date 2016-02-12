a7a0fe38b3d7d7c78292cda9587d2dc7a00229f0
Martin A. Brown <martin@linux-ip.net>
2016-02-12 13:25:19 -0800

    adding two unused doctypes

---
+++ tldp/doctypes/asciidoc.py
@@ -0,0 +1,25 @@
+#! /usr/bin/python
+
+from ..utils import logger
+
+
+class Asciidoc(object):
+    formatname = 'AsciiDoc'
+    extensions = ['.txt']
+    signatures = []
+    tools = ['asciidoc', 'a2x']
+
+    def create_txt(self):
+        logger.info("Creating txt for %s", self.source.stem)
+
+    def create_pdf(self):
+        logger.info("Creating PDF for %s", self.source.stem)
+
+    def create_html(self):
+        logger.info("Creating chunked HTML for %s", self.source.stem)
+
+    def create_htmls(self):
+        logger.info("Creating single page HTML for %s", self.source.stem)
+
+#
+# -- end of file
