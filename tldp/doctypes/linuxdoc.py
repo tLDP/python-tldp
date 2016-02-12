5263a3a219992c2a9e322d226cf981f24abfde7e
Martin A. Brown <martin@linux-ip.net>
2016-02-12 12:49:50 -0800

    fleshing out the linuxdoc processing commands

--- tldp/doctypes/linuxdoc.py
+++ tldp/doctypes/linuxdoc.py
@@ -4,36 +4,42 @@ from __future__ import absolute_import, division, print_function
 
 import os
 
-from ..utils import logger, runner
+from ..utils import logger
 from .common import SignatureChecker
 
 
 class Linuxdoc(SignatureChecker):
+    formatname = 'Linuxdoc'
     extensions = ['.sgml']
     signatures = ['<!doctype linuxdoc system', ]
+    tools = ['sgml2html', 'html2text', 'htmldoc']
 
-    def __init__(self, source, output, runner):
-        self.source = source
-        self.output = output
-        self.runner = runner
+    def __init__(self, *args, **kwargs):
+        self.source = kwargs.get('source')
+        self.output = kwargs.get('output')
+        super(Linuxdoc, self).__init__()
 
     def create_txt(self):
-        cmd = ['html2text', '-style', 'pretty', '-nobs',
+        cmd = [self.html2text, '-style', 'pretty', '-nobs',
                self.output.htmls_name]
         stdout = self.output.txt_name
 
     def create_pdf(self):
-        cmd = ['htmldoc', '--size', 'universal', '-t', 'pdf',
+        logger.info("Creating PDF for %s", self.source.stem)
+        cmd = [self.htmldoc, '--size', 'universal', '-t', 'pdf',
                '--firstpage', 'p1', '--outfile', self.output.pdf_name,
                self.output.htmls_name]
 
     def create_html(self):
-        success = False
+        logger.info("Creating chunked HTML for %s", self.source.stem)
+        cmd = [self.sgml2html, self.source.filename]
         try:
-            cmd = ['sgml2html', self.source.filename]
-
+            # -- execute cmd
+            pass
         except OSError:
-            success = False
+            return False
+
+        success = False
         try:
             opwd = os.getcwd()
             os.chdir(self.output.dirname)
@@ -41,11 +47,11 @@ class Linuxdoc(SignatureChecker):
             os.chdir(opwd)
             success = True
         except OSError:
-            success = False
+            pass
         return success
 
     def create_htmls(self):
-        cmd = ['sgml2html', '--split=0', self.source.filename]
+        cmd = [self.sgml2html, '--split=0', self.source.filename]
         os.rename(self.output.html_name, self.output.htmls_name)
 
 #
