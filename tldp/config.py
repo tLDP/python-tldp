04c10a6af9090fa07c0d62c5e953d35611a03983
Martin A. Brown <martin@linux-ip.net>
2016-02-23 08:59:37 -0800

    refer to %(default)s; shorter lines

--- tldp/config.py
+++ tldp/config.py
@@ -13,58 +13,49 @@ from tldp.inventory import status_classes
 
 
 def collectconfiguration(tag, argv):
-    argparser = DefaultFreeArgumentParser()
-    argparser.add_argument('--build',
-                           '-b',
-                           nargs='*', default=None, type=str,
-                           help='build LDP documentation')
-    argparser.add_argument('--detail', '--list',
-                           '-l',
-                           default=None, type=str,
-                           choices=status_classes.keys(),
-                           help='list elements of LDP publication system')
-    argparser.add_argument('--status', '--summary',
-                           '-t',
-                           action='store_true', default=False,
-                           help='produce a status report of the inventory')
-    argparser.add_argument('--verbose',
-                           '-v',
-                           action='store_true', default=False,
-                           help='increase information produced during --list')
-    argparser.add_argument('--loglevel',
-                           '-L',
-                           default=logging.ERROR, type=isloglevel,
-                           help='set the loglevel')
-    argparser.add_argument('--sourcedir', '--source-dir', '--source-directory',
-                           '-s',
-                           action='append', default='', type=isdirectory,
-                           help='a directory containing LDP source documents')
-    argparser.add_argument('--pubdir', '--output', '--outputdir', '--outdir',
-                           '-o',
-                           default=None, type=isdirectory,
-                           help='a directory containing LDP output documents')
-    argparser.add_argument('--configfile', '--config-file', '--cfg',
-                           '-c',
-                           default=None, type=isdirectory,
-                           help='a configuration file')
+    ap = DefaultFreeArgumentParser()
+    ap.add_argument('--build',
+                    '-b',
+                    action='store_true', default=False,
+                    help='build LDP documentation [%(default)s]')
+    ap.add_argument('--detail', '--list',
+                    '-l',
+                    action='store_true', default=False,
+                    help='list elements of LDP system [%(default)s]')
+    ap.add_argument('--status', '--summary',
+                    '-t',
+                    action='store_true', default=False,
+                    help='dump inventory status report [%(default)s]')
+    ap.add_argument('--verbose',
+                    action='store_true', default=False,
+                    help='more info in --list and --status [%(default)s]')
+    ap.add_argument('--loglevel',
+                    '-L',
+                    default=logging.ERROR, type=isloglevel,
+                    help='set the loglevel')
+    ap.add_argument('--sourcedir', '--source-dir', '--source-directory',
+                    '-s',
+                    action='append', default='', type=isdirectory,
+                    help='a directory containing LDP source documents')
+    ap.add_argument('--pubdir', '--output', '--outputdir', '--outdir',
+                    '-o',
+                    default=None, type=isdirectory,
+                    help='a directory containing LDP output documents')
+    ap.add_argument('--configfile', '--config-file', '--cfg',
+                    '-c',
+                    default=None, type=isdirectory,
+                    help='a configuration file')
 
     # -- collect up the fragments of CLI; automate detection?
     #
-    tldp.doctypes.linuxdoc.config_fragment(argparser)
-    tldp.doctypes.docbooksgml.config_fragment(argparser)
-    tldp.doctypes.docbook4xml.config_fragment(argparser)
-    tldp.doctypes.docbook5xml.config_fragment(argparser)
+    tldp.doctypes.linuxdoc.config_fragment(ap)
+    tldp.doctypes.docbooksgml.config_fragment(ap)
+    tldp.doctypes.docbook4xml.config_fragment(ap)
+    tldp.doctypes.docbook5xml.config_fragment(ap)
 
-    cc = CascadingConfig(tag, argparser, argv)
+    cc = CascadingConfig(tag, ap, argv)
     config, args = cc.parse()
-    return config
-
-
-def main(argv):
-    config = collectconfiguration('ldptool', argv)
-    import pprint
-    pprint.pprint(vars(config))
-    return 0
+    return config, args
 
 #
 # -- end of file
