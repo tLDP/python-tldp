7486a26cc3643e086467004f4f314832eb72a14c
Martin A. Brown <martin@linux-ip.net>
2016-02-22 16:20:15 -0800

    initial commit of main 'ldptool' driver program

---
+++ tldp/driver.py
@@ -0,0 +1,27 @@
+#! /usr/bin/python
+# -*- coding: utf8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+import sys
+
+from .config import collectconfiguration
+
+
+def list(config):
+    pass
+
+
+def status(config):
+    pass
+
+
+def build(config):
+    pass
+
+
+def publish(config):
+    pass
+
+#
+# -- end of file
