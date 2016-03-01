363bba8a37e1e59654f068f18f4095f8ed8ecacd
Martin A. Brown <martin@linux-ip.net>
2016-02-29 20:32:36 -0800

    pep8/pyflakes

--- tldp/config.py
+++ tldp/config.py
@@ -3,36 +3,37 @@
 
 from __future__ import absolute_import, division, print_function
 
-import inspect
 import logging
-logger = logging.getLogger(__name__)
 
 from tldp.utils import arg_isdirectory, arg_isloglevel, arg_isreadablefile
 from tldp.cascadingconfig import CascadingConfig, DefaultFreeArgumentParser
-from tldp.inventory import status_classes
 
 import tldp.typeguesser
+
+logger = logging.getLogger(__name__)
 
 
 def collectconfiguration(tag, argv):
     ap = DefaultFreeArgumentParser()
+
     g = ap.add_mutually_exclusive_group()
     g.add_argument('--build',
-                    '-b',
-                    action='store_true', default=False,
-                    help='build LDP documentation [%(default)s]')
+                   '-b',
+                   action='store_true', default=False,
+                   help='build LDP documentation [%(default)s]')
     g.add_argument('--script',
-                    '-S',
-                    action='store_true', default=False,
-                    help='dump runnable script [%(default)s]')
+                   '-S',
+                   action='store_true', default=False,
+                   help='dump runnable script [%(default)s]')
     g.add_argument('--detail', '--list',
-                    '-l',
-                    action='store_true', default=False,
-                    help='list elements of LDP system [%(default)s]')
+                   '-l',
+                   action='store_true', default=False,
+                   help='list elements of LDP system [%(default)s]')
     g.add_argument('--summary',
-                    '-t',
-                    action='store_true', default=False,
-                    help='dump inventory status report [%(default)s]')
+                   '-t',
+                   action='store_true', default=False,
+                   help='dump inventory status report [%(default)s]')
+
     ap.add_argument('--verbose',
                     action='store_true', default=False,
                     help='more info in --list and --status [%(default)s]')
