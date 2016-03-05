f617cca3d3455b47798697b76ea481f6677d71ee
Martin A. Brown <martin@linux-ip.net>
2016-03-04 17:10:21 -0800

    add support for Asciidoc detection

--- tldp/doctypes/__init__.py
+++ tldp/doctypes/__init__.py
@@ -1,6 +1,7 @@
 # from .text import Text
 # from .rst import RestructuredText
 # from .markdown import Markdown
+from .asciidoc import Asciidoc
 from .linuxdoc import Linuxdoc
 from .docbooksgml import DocbookSGML
 from .docbook4xml import Docbook4XML
