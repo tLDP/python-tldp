2d75d3c4dec3b4d56156e1a8c34e8a7f5cf3c09c
Martin A. Brown <martin@linux-ip.net>
2016-03-09 18:08:18 -0800

    remove old boilerplate from markdown and rst stock code

--- tldp/doctypes/example.py
+++ tldp/doctypes/example.py
@@ -14,19 +14,6 @@ class Frobnitz(BaseDoctype):
     formatname = 'Frobnitz'
     extensions = ['.fb']
     signatures = ['{{Frobnitz-Format 2.3}}']
-    tools = ['executablename', 'another']
-
-    def create_txt(self):
-        logger.info("Creating txt for %s", self.source.stem)
-
-    def create_pdf(self):
-        logger.info("Creating PDF for %s", self.source.stem)
-
-    def create_html(self):
-        logger.info("Creating chunked HTML for %s", self.source.stem)
-
-    def create_htmls(self):
-        logger.info("Creating single page HTML for %s", self.source.stem)
 
 #
 # -- end of file
