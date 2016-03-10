c581980aaf90dfc50ee2a1c75dad5965c1692843
Martin A. Brown <martin@linux-ip.net>
2016-03-09 18:07:52 -0800

    remove old boilerplate from markdown and rst stock code

--- tldp/doctypes/markdown.py
+++ tldp/doctypes/markdown.py
@@ -16,17 +16,5 @@ class Markdown(BaseDoctype):
     signatures = []
     tools = ['pandoc']
 
-    def create_txt(self):
-        logger.info("Creating txt for %s", self.source.stem)
-
-    def create_pdf(self):
-        logger.info("Creating PDF for %s", self.source.stem)
-
-    def create_html(self):
-        logger.info("Creating chunked HTML for %s", self.source.stem)
-
-    def create_htmls(self):
-        logger.info("Creating single page HTML for %s", self.source.stem)
-
 #
 # -- end of file
--- tldp/doctypes/rst.py
+++ tldp/doctypes/rst.py
@@ -14,19 +14,7 @@ class RestructuredText(BaseDoctype):
     formatname = 'reStructuredText'
     extensions = ['.rst']
     signatures = []
-    tools = ['rst2html']
 
-    def create_txt(self):
-        logger.info("Creating txt for %s", self.source.stem)
-
-    def create_pdf(self):
-        logger.info("Creating PDF for %s", self.source.stem)
-
-    def create_html(self):
-        logger.info("Creating chunked HTML for %s", self.source.stem)
-
-    def create_htmls(self):
-        logger.info("Creating single page HTML for %s", self.source.stem)
 
 #
 # -- end of file
