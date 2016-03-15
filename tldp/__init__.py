a2daee9425acb0e612d23f189ed19fce37b27af1
Martin A. Brown <martin@linux-ip.net>
2016-03-14 20:32:09 -0700

    use absolute_import here, too (Python 3)

--- tldp/__init__.py
+++ tldp/__init__.py
@@ -1,4 +1,8 @@
-import config
-import outputs
-import sources
-import inventory
+# -*- coding: utf8 -*-
+
+from __future__ import absolute_import
+
+import tldp.config
+import tldp.outputs
+import tldp.sources
+import tldp.inventory
