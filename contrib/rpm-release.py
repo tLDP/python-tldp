211d3b93d7368f910c2b0ef83c5edec5067cd317
Martin A. Brown <martin@linux-ip.net>
2016-04-27 14:03:43 -0400

    find the parent ./tldp/ directory

--- contrib/rpm-release.py
+++ contrib/rpm-release.py
@@ -7,13 +7,17 @@ from __future__ import print_function
 import os
 import sys
 
+opd = os.path.dirname
+opj = os.path.join
+
+sys.path.append(opd(opd(__file__)))
 from tldp import VERSION
 
-fin = open(os.path.join(os.path.dirname(__file__), 'tldp.spec.in'))
-fout = open(os.path.join(os.path.dirname(__file__), 'tldp.spec'))
+fin = open(opj(opd(__file__), 'tldp.spec.in'))
+fout = open(opj(opd(__file__), 'tldp.spec'), 'w')
 
 def transform(mapping, text):
-    for tag, replacement in mapping.iteritems():
+    for tag, replacement in mapping.items():
         text = text.replace(tag, replacement)
     return text
 
