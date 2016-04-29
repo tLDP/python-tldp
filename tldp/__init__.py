8a554a430da617e96ab50d5c3fd71d9ac224f023
Martin A. Brown <martin@linux-ip.net>
2016-04-29 11:02:02 -0400

    adding 2016 LDP copyright to each file

--- tldp/__init__.py
+++ tldp/__init__.py
@@ -1,4 +1,6 @@
 # -*- coding: utf8 -*-
+#
+# Copyright (c) 2016 Linux Documentation Project
 
 from __future__ import absolute_import
 
--- tldp/typeguesser.py
+++ tldp/typeguesser.py
@@ -1,5 +1,7 @@
 #! /usr/bin/python
 # -*- coding: utf8 -*-
+#
+# Copyright (c) 2016 Linux Documentation Project
 
 from __future__ import absolute_import, division, print_function
 
