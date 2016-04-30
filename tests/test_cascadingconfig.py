d16be1b0763415c79cbf160c75979d527068400f
Martin A. Brown <martin@linux-ip.net>
2016-04-30 17:49:20 -0400

    adding 2016 LDP copyright to each file

--- tests/test_cascadingconfig.py
+++ tests/test_cascadingconfig.py
@@ -1,3 +1,6 @@
+# -*- coding: utf8 -*-
+#
+# Copyright (c) 2016 Linux Documentation Project
 
 from __future__ import absolute_import, division, print_function
 from __future__ import unicode_literals
